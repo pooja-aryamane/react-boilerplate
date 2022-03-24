160cb8c78d90e4c443750a3a3f419fa2f44a0d13
Pooja Aryamane <70020869+pooja-aryamane@users.noreply.github.com>
2022-03-24 19:38:03 +0530

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import { NativeModules } from 'react-native'
+// name as defined via ReactContextBaseJavaModule's getName
+@Override
+public String getName() {
+    return "react-native-boilerplate";
+}
+
+
+module.exports = NativeModules.Boilerplate
+
